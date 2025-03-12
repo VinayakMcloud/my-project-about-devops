bb4b21a3e4e6d51d371887a65dc5b1872d7405b7
VinayakMcloud <vinayakmishra360@gmail.com>
2025-03-12 19:41:03 +0530

    Add files via upload

---
+++ IAM/varible.tf
@@ -0,0 +1,9 @@
+variable "access_key" {     
+    description = "AKIASE5KQ2MMDYFU7GUH"       
+}           
+variable "secret_key" {     
+    description = "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"      
+}       
+variable "region" {
+    description = "us-east-1"          
+}
