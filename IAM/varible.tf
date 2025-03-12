2b6003db3c42adc326f7ff6d1c36f9febd686605
VinayakMcloud <vinayakmishra360@gmail.com>
2025-03-12 19:50:28 +0530

    Delete IAM directory

--- IAM/varible.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {     
-    description = "AKIASE5KQ2MMDYFU7GUH"       
-}           
-variable "secret_key" {     
-    description = "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"      
-}       
-variable "region" {
-    description = "us-east-1"          
-}
